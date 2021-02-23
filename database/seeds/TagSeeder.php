638cda0ce209dba784726c8d8ec68aea372bad08
salvoab <salvatore.abate85@gmail.com>
2021-02-23 18:34:21 +0100

    Model migration e seeder per Tag

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Tag;
+
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) { 
+            $newTag = new Tag();
+            $newTag->name = $faker->word();
+            $newTag->description = $faker->paragraph();
+            $newTag->save();
+        }
+    }
+}
