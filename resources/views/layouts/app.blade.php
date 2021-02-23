0c0371e1a9d2c6d081d52a14a2614f3034a9c046
salvoab <salvatore.abate85@gmail.com>
2021-02-23 19:08:42 +0100

    Cambiata la navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,24 +11,30 @@
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand navbar-light bg-light">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('articles.index') }}">Articoli</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('articles_api') }}">Articoli da API</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('categories_api') }}">Categorie da API</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tags_api') }}">Tag da API</a>
-                    </li>
-                </ul>
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <a class="navbar-brand" href="{{ route('home') }}">Boolpress</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('articles.index') }}">Articoli</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Da API</a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownId">
+                                <a class="dropdown-item" href="{{ route('articles_api') }}">Articoli da API</a>
+                                <a class="dropdown-item" href="{{ route('categories_api') }}">Categorie da API</a>
+                                <a class="dropdown-item" href="{{ route('tags_api') }}">Tag da API</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
         <div class="container">
