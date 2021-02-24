ea56337ee387b933d53117325b3ba1367ea900f0
salvoab <salvatore.abate85@gmail.com>
2021-02-24 16:33:22 +0100

    Nome della categoria visualizzato in view Show dell'articolo

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,6 +8,7 @@ Articolo #{{$article->id}}
 
   <div class="jumbotron">
     <h1>{{$article->title}}</h1>
+    <p class="lead">Articolo nella Categoria: {{ $article->category->name }}</p>
     <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-primary">Modifica questo articolo</a>
   </div>  
     
