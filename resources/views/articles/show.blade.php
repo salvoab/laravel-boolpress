8526637bebbf6ea56d1f11a2c5c88a5fad380e26
salvoab <salvatore.abate85@gmail.com>
2021-02-26 15:37:11 +0100

    Tag associati ad un post mostrati in articles/show view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,6 +9,15 @@ Articolo #{{$article->id}}
   <div class="jumbotron">
     <h1>{{$article->title}}</h1>
     <p class="lead">Articolo nella Categoria: {{ $article->category->name }}</p>
+    <p class="lead">Tags: 
+      @if(count($article->tags) > 0)
+        @foreach($article->tags as $tag)
+          <span>{{$tag->name}}</span>
+        @endforeach
+      @else
+        <span>Non ci sono tag associati a questo post</span>
+      @endif
+    </p>
     <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-primary">Modifica questo articolo</a>
   </div>  
     
